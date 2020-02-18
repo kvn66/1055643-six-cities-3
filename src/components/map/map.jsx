14007558c15c2b7128003ae20ebc815c53fa2388
kvn66 <vadim.kalashnikov66@gmail.com>
2020-02-18 19:46:35 +0500

    Добавил тесты

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -14,32 +14,33 @@ export default class Map extends PureComponent {
     const {location} = this.props;
     const {cityCoordinates, places} = location;
 
+    if (this._mapRef.current) {
+      const icon = leaflet.icon({
+        iconUrl: `/img/pin.svg`,
+        iconSize: [30, 30]
+      });
+      const zoom = 12;
 
-    const icon = leaflet.icon({
-      iconUrl: `/img/pin.svg`,
-      iconSize: [30, 30]
-    });
-    const zoom = 12;
+      this.map = leaflet.map(this._mapRef.current, {
+        center: cityCoordinates,
+        zoom,
+        zoomControl: false,
+        marker: true
+      });
+      this.map.setView(cityCoordinates, zoom);
 
-    this.map = leaflet.map(this._mapRef.current, {
-      center: cityCoordinates,
-      zoom,
-      zoomControl: false,
-      marker: true
-    });
-    this.map.setView(cityCoordinates, zoom);
-
-    leaflet
-      .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-        attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
-      })
-      .addTo(this.map);
-
-    places.forEach((place) => {
       leaflet
-        .marker(place.coordinates, {icon})
+        .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+          attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+        })
         .addTo(this.map);
-    });
+
+      places.forEach((place) => {
+        leaflet
+          .marker(place.coordinates, {icon})
+          .addTo(this.map);
+      });
+    }
   }
 
   componentWillUnmount() {
