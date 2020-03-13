7c8e44ffe555625b302671b39e94c86c6748f5a8
Vadim Kalashnikov <vadim.kalashnikov66@gmail.com>
2020-03-13 16:56:15 +0500

    Убрал рефы

--- src/components/review-form/review-form.jsx
+++ src/components/review-form/review-form.jsx
@@ -1,140 +1,104 @@
-import React, {createRef} from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {Operation as ReviewsOperation, ActionCreator} from "../../reducers/reviews/reviews";
 import {connect} from "react-redux";
 import {shakeElement} from "../../utils";
 import {getFormIsLocked, getButtonIsLocked, getComment, getRating} from "../../reducers/reviews/selectors";
-import {NameSpace} from "../../reducers/name-space";
+import {MemoizedReviewFormRatingItem} from "../review-form-rating-item/review-form-rating-item.jsx";
 
 const RADIX = 10;
+const RATING_TITLES = [`terribly`, `badly`, `not bad`, `good`, `perfect`];
 
-const ReviewForm = (props) => {
-  const {cardId, formIsLocked, buttonIsLocked, rating, comment, sendReview, setFormLockState, setButtonLockState, setRating, setComment} = props;
-  const formRef = createRef();
+class ReviewForm extends PureComponent {
+  constructor(props) {
+    super(props);
 
-  const setButtonState = () => {
-    const buttonState = !(rating > 0 && comment.length > 5 && comment.length < 500);
+    this.changeRatingHandler = this.changeRatingHandler.bind(this);
+    this.changeTextHandler = this.changeTextHandler.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
+    this.onSuccess = this.onSuccess.bind(this);
+    this.onError = this.onError.bind(this);
+  }
+
+  setButtonState() {
+    const {rating, comment, setButtonLockState} = this.props;
+    const buttonState = !(rating > 0 && comment.length > 50 && comment.length < 500);
     setButtonLockState(buttonState);
-    console.log(buttonState);
-    console.log(rating);
-    console.log(comment);
-    console.log(comment.length);
-  };
+  }
 
-  const lockForm = () => {
-    formRef.current.querySelectorAll(`input, textarea, button`)
-      .forEach((elem) => elem.setAttribute(`disabled`, `disabled`));
-  };
-
-  const unlockForm = () => {
-    formRef.current.querySelectorAll(`input, textarea, button`)
-      .forEach((elem) => elem.removeAttribute(`disabled`, `disabled`));
-  };
-
-  const clearForm = () => {
-    formRef.current.reset();
+  clearForm() {
+    const {setRating, setComment} = this.props;
     setRating(0);
     setComment(``);
-    setButtonState();
-  };
+  }
 
-  const onSuccess = () => {
-    unlockForm();
-    clearForm();
-  };
+  onSuccess() {
+    this.clearForm();
+  }
 
-  const onError = () => {
-    shakeElement(formRef.current);
-    unlockForm();
-  };
+  onError() {
+    // shakeElement(formRef.current);
+  }
 
-  const changeRatingHandler = (evt) => {
+  changeRatingHandler(evt) {
+    const {setRating} = this.props;
     setRating(parseInt(evt.target.value, RADIX));
-    setButtonState();
-  };
+  }
 
-  const changeTextHandler = (evt) => {
+  changeTextHandler(evt) {
+    const {setComment} = this.props;
     setComment(evt.target.value);
-    setButtonState();
-  };
+  }
 
-  const submitHandler = (evt) => {
+  submitHandler(evt) {
+    const {cardId, rating, comment, sendReview} = this.props;
     evt.preventDefault();
     const commentPost = {
       rating,
       comment
     };
 
-    lockForm();
-    sendReview(cardId, commentPost, onSuccess, onError);
-  };
+    sendReview(cardId, commentPost, this.onSuccess, this.onError);
+  }
 
-  return (
-    <form ref={formRef} onSubmit={submitHandler} className="reviews__form" action="#" method="post">
-      <label className="reviews__label form__label" htmlFor="review">Your review</label>
-      <div className="reviews__rating-form form__rating">
-        <input onChange={changeRatingHandler}
-          className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio"
-        />
-        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"/>
-          </svg>
-        </label>
+  componentDidUpdate() {
+    this.setButtonState();
+  }
 
-        <input onChange={changeRatingHandler}
-          className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio"
-        />
-        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"/>
-          </svg>
-        </label>
+  render() {
+    const {formIsLocked, buttonIsLocked, rating, comment} = this.props;
 
-        <input onChange={changeRatingHandler}
-          className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars"
-          type="radio"
-        />
-        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"/>
-          </svg>
-        </label>
+    const ratingElement = RATING_TITLES.map((title, index) =>
+      <MemoizedReviewFormRatingItem key={index} id={5 - index} title={title} rating={rating} formIsLocked={formIsLocked} onChangeRating={this.changeRatingHandler} />
+    );
 
-        <input onChange={changeRatingHandler}
-          className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars"
-          type="radio"
+    return (
+      <form onSubmit={this.submitHandler} className="reviews__form" action="#" method="post">
+        <label className="reviews__label form__label" htmlFor="review">Your review</label>
+        <div className="reviews__rating-form form__rating">
+          {ratingElement}
+        </div>
+        <textarea
+          onChange={this.changeTextHandler}
+          className="reviews__textarea form__textarea" id="review" name="review"
+          placeholder="Tell how was your stay, what you like and what can be improved"
+          disabled={formIsLocked}
+          value={comment}
         />
-        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"/>
-          </svg>
-        </label>
-
-        <input onChange={changeRatingHandler}
-          className="form__rating-input visually-hidden" name="rating" value="1" id="1-star"
-          type="radio"
-        />
-        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"/>
-          </svg>
-        </label>
-      </div>
-      <textarea onChange={changeTextHandler}
-        className="reviews__textarea form__textarea" id="review" name="review"
-        placeholder="Tell how was your stay, what you like and what can be improved"
-      />
-      <div className="reviews__button-wrapper">
-        <p className="reviews__help">
-          To submit review please make sure to set <span className="reviews__star">rating</span> and
-          describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
-        </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled={buttonIsLocked}>Submit</button>
-      </div>
-    </form>
-  );
-};
+        <div className="reviews__button-wrapper">
+          <p className="reviews__help">
+            To submit review please make sure to set <span className="reviews__star">rating</span> and
+            describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+          </p>
+          <button
+            className="reviews__submit form__submit button" type="submit" disabled={buttonIsLocked || formIsLocked}
+          >Submit
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
 
 ReviewForm.propTypes = {
   cardId: PropTypes.number.isRequired,
@@ -150,12 +114,11 @@ ReviewForm.propTypes = {
 };
 
 const mapStateToProps = (state) => {
-  console.log(state.REVIEWS);
   return {
     formIsLocked: getFormIsLocked(state),
     buttonIsLocked: getButtonIsLocked(state),
     rating: getRating(state),
-    comment: state[NameSpace.REVIEWS].comment,
+    comment: getComment(state),
   };
 };
 
